21fc7c00b035f85cf9074106a80724ec6d254e71
ruoyangchen <135175852+ruoyangchen@users.noreply.github.com>
2025-01-07 13:58:25 -0800

    update title
    update title

--- Components/Intro.jsx
+++ Components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Intro(props) {
     return (
         <div className="intro">
-            <h1>Art Snob Quiz</h1>
+            <h1>Art Pop Quiz</h1>
             <h2>Test your knowledge on art!</h2>
             <button 
             className='btn start-btn'
